bf9110eb3a59649389270b57e45d190ae05c7bfc
DadhaniyaSomin <somindadhaniya111@gmail.com>
2023-12-18 17:19:53 +0530

    Route implementation 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
+import AuthLayout from './_auth/AuthLayout';
+import SignUpForm from './_auth/forms/SignUpForm';
+import SigninForm from './_auth/forms/SigninForm';
+import RootLayout from './_root/RootLayout';
+import Home from './_root/pages/Home';
 import './globals.css';
 import { Routes, Route } from 'react-router-dom';
 // import SigninForm from './'
@@ -6,11 +11,14 @@ const App = () => {
     <main className="flex h-screen">
       <Routes>
         {/* public */}
-        <Route path="sign-in" element={<SigninForm/>} />
-        <Route path="sign-up" element={<SignUpForm/>} />
-        
+        <Route element={<AuthLayout/> } > 
+          <Route path="sign-in" element={<SigninForm/>} />
+          <Route path="sign-up" element={<SignUpForm/>} />
+        </Route>
         {/* private routes */}
-        <Route index element={<Home/>} />
+        <Route  element={<RootLayout/>}>
+            <Route index element={<Home />} />
+        </Route>
       </Routes>
     </main>
   );
